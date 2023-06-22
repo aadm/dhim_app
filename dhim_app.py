9f600fe5162132b38df6819949777a64219f41d6
Alessandro Amato del Monte <alessandro.adm@gmail.com>
2023-06-22 19:14:04 +0200

    initial commit

---
+++ dhim_app.py
@@ -0,0 +1,204 @@
+# dhimatrix_app
+#
+# $ streamlit run dhim_app.py
+#
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+import numpy as np
+import pandas as pd
+import streamlit as st
+import altair as alt
+from dhim_funcs import *
+
+#==========================================================
+# initialize sidebar
+
+grade_options = ['Random', 'Predefined', 'Best', 'Worst']
+pos_options = ['Random', 'Predefined']
+
+with st.sidebar:
+    st.write('_Predefined_ fills in probabilities and grades with default values and allows the user to modify values.')
+    sel_grade_opt = st.selectbox('Grades', grade_options, index=1)
+    sel_pos_opt = st.selectbox('POS', pos_options, index=1)
+
+if sel_grade_opt == 'Random':
+    g_presel = np.random.choice(grade_types, size=24)
+elif sel_grade_opt == 'Predefined':
+    g_presel = ['A','B','C','A','D','B','B','D','C','C','B','D','C','C','B','C','B','B','B','A','A','A','B','B']
+elif sel_grade_opt == 'Best':
+    g_presel = ['D']*24
+elif sel_grade_opt == 'Worst':
+    g_presel = ['A']*24
+
+if sel_pos_opt == 'Random':
+    p_presel = np.random.choice(np.arange(0.3,1.,.1), size=5)
+elif sel_pos_opt == 'Predefined':
+    p_presel = [.9, .9, .7, .4, .5]
+
+#==========================================================
+# initialize widgets
+
+grade_types = ['A', 'B', 'C', 'D']
+weight_types = ['W1', 'W2', 'W3']
+
+riskelem = ['RES', 'SEA', 'SRC', 'TRP', 'CHG']
+
+# assign bmt names, ids, extended descriptions
+dhim_names = {
+    # BACKGROUND TAB
+    'DHI_BACK_1': ['BUR', 'Burial depth'],
+    'DHI_BACK_2': ['PORO', 'Porosity range'],
+    'DHI_BACK_3': ['FLUID', 'HC type'],
+    'DHI_BACK_4': ['AGE', 'Age of target'],
+    'DHI_BACK_5': ['DHIP', 'DHI play'],
+    'DHI_BACK_6': ['SFACIES', 'Seismic facies'],
+    'DHI_BACK_7': ['PITFALL', 'Possible pitfalls'],
+    # DATA QUALITY TAB
+    'DHI_DQ_1': ['SEISD', 'Seismic database'],
+    'DHI_DQ_2': ['SEISQ', 'Seismic data quality'],
+    'DHI_DQ_3': ['RPM', 'Rock physics model'],
+    'DHI_DQ_4': ['CLASS', 'AVO class'],
+    # AMPLITUDE ANOMALY TAB
+    'DHI_AA_1': ['AMPC', 'Amplitude change'],
+    'DHI_AA_2': ['ACONS', 'Amplitude consistency'],
+    'DHI_AA_3': ['MODM', 'Seismic signature'],
+    'DHI_AA_4': ['STRFIT', 'Amplitude structural fit'],
+    # AVO TAB
+    'DHI_AVO_1': ['AVOM', 'AVO analysis method'],
+    'DHI_AVO_2': ['AVOA', 'AVO change & consistency'],
+    'DHI_AVO_3': ['AVOVM', 'AVO signature'],
+    'DHI_AVO_4': ['AVOFIT', 'AVO structural fit'],
+    # OTHER DHIS TAB
+    'DHI_OTHER_1': ['FSPOT', 'Flat spot'],
+    'DHI_OTHER_2': ['PHASE', 'Phase change'],
+    'DHI_OTHER_3': ['LOWF', 'Low frequency anomaly'],
+    'DHI_OTHER_4': ['PULLD', 'Pull-down'],
+    'DHI_OTHER_5': ['CHIM', 'Gas chimneys'],
+}
+    
+
+# POS widgets
+# p1, p2, p3, p4, p5 = st.columns(5, gap='medium')
+
+pos_widgets = st.columns(5, gap='medium')
+# tmp = np.zeros(len(p_presel))
+tmp = np.full(len(p_presel), np.nan)
+opt0 = dict(min_value=0.0, max_value=1.0, step=0.05, format='%.2f')
+for i, val in enumerate(pos_widgets):
+    with val:
+        tmp[i] = st.number_input(riskelem[i], value=p_presel[i], **opt0)
+
+pos_input = tmp
+
+st.divider()
+
+# DHI Matrix characteristics widgets
+
+grade_widgets = st.columns(3, gap='medium')
+# tmp = ['A']*(len(g_presel))
+# tmp = ['A']*(len(g_presel))
+tmp = np.full(len(g_presel), 'X')
+
+blocks = ['**Background and Data**', '**Full stack and AVO**', '**Other DHIs**']
+rr = [[0,11], [11,19], [19,24]]
+dck = list(dhim_names.keys())
+
+c = 0 
+
+opt1 = dict(options=grade_types, horizontal=True)
+for i, colg in enumerate(grade_widgets):
+    with colg:
+        subset = dck[rr[i][0]:rr[i][1]]
+        st.write(blocks[i])
+        for j, val in enumerate(subset):
+            id = subset[j]    
+            name = dhim_names[id][0]    
+            tmp[c] = st.radio(name, index=grade_types.index(g_presel[c]), **opt1)
+            c += 1
+
+grades = tmp
+
+st.divider()
+
+#==========================================================
+# run calculations
+
+# update dhi matrix DataFrame with selected grades
+dm = initialize_dhimatrix()
+dm_updated, pdhi_idx, pitf1_idx, pitf2_idx, pitf_idx = update_dhi_matrix(dm, grades)
+
+# calculate final POS
+posg, pos, prb = update_pos(pos_input, pdhi_idx, pitf_idx)
+
+#==========================================================
+# summary tabs
+
+# define prior and plot bayes posterior for all 5 risk model elements
+prior = np.linspace(0, 1.)
+tmp = np.zeros((prior.size, 5+1))
+tmp[:, 0] = prior
+for i in range(5):
+    tmp[:, i+1] = posterior_probability(prior, prb['sens'].iloc[i], prb['spec'].iloc[i])
+
+col_names = list(prb.index)
+col_names.insert(0, 'PRIOR')
+df = pd.DataFrame(tmp, columns = col_names)
+df0 = df.melt('PRIOR', var_name='Risk Model Element', value_name='POSTERIOR')
+
+# assemble input POS into a dataframe for plotting
+points = pd.DataFrame.from_dict(dict(zip(prb.index, prb.posg)), orient='index', columns=['posg'])
+points['pos']  = prb.pos
+points = points.reset_index()
+
+# hard-code upper and lower bounds for this version of the DHI Matrix
+bayes_upper_bound = posterior_probability(prior, 0.804, 0.897)
+bayes_lower_bound = posterior_probability(prior, 0.731, 0.275)
+
+bounds = pd.DataFrame.from_dict({
+    'PRIOR': prior,
+    'Lower': bayes_lower_bound,
+    'Upper':bayes_upper_bound})
+
+# define list of 5 colors
+colrs = alt.Scale(range=['olive', 'green', 'darkgrey','red', 'magenta'])
+
+# setup altair charts
+c1 = alt.Chart(df0).mark_line().encode(
+    x='PRIOR:Q',
+    y='POSTERIOR:Q',
+    color=alt.Color('Risk Model Element:N', scale=colrs)
+)
+
+c2 = alt.Chart(points).mark_point().encode(
+    x='posg:Q',
+    y='pos:Q',
+    color=alt.Color('index:N', scale=colrs)
+)
+
+opt = dict(filled=True, opacity=0.5, color='black')
+c3 = alt.Chart(bounds).mark_point(**opt).encode(
+    x='PRIOR:Q',
+    y='Upper:Q')
+
+c4 = alt.Chart(bounds).mark_point(**opt).encode(
+    x='PRIOR:Q',
+    y='Lower:Q')
+
+chart = c1+c2+c3+c4
+
+# setup tabs
+tab1, tab2, = st.tabs(['output', 'plot'])
+
+with tab1:
+    col1, col2 = st.columns(2, gap='large')
+    with col1:
+        st.write('### POSg: :red[{:.2f}] // POS: :red[{:.2f}]'.format(posg, pos))
+        st.write('Primary DHI Index: :red[{:.2f}]'.format(pdhi_idx))
+        txt = 'Pitfall Index 1: :red[{:.2f}] // Index 2: :red[{:.2f}]'
+        st.write(txt.format(pitf1_idx, pitf2_idx))
+        st.write('Pitfall Index avg: :red[{:.2f}]'.format(pitf_idx))
+    with col2:
+        st.dataframe(prb, use_container_width=True)
+    st.dataframe(dm_updated, use_container_width=True, hide_index=True, height=800)
+
+with tab2:
+    st.altair_chart(chart, use_container_width=True, theme="streamlit")
---
+++ dhim_funcs.py
@@ -0,0 +1,305 @@
+#----------------------------------------------------------
+# aadm 2022-2023
+# last update: 2023-06-22
+#----------------------------------------------------------
+
+
+
+def marginal_probability(prior, sens, spec):
+    '''
+    Calculate marginal probability of A (test, presence of DHIs)
+    i.e. probability of having DHIs at all, wether or not hydrocarbons are present
+    '''
+    return sens*prior + (1-spec)*(1-prior) # P(B)
+
+
+def posterior_probability(prior, sens, spec):
+    '''
+    Calculate posterior probability P(H|A)
+    i.e. prob of event H (presence of hydrocarbon) being true
+    if A (test, presence of DHIs) is true
+    '''
+    return (sens*prior) / marginal_probability(prior, sens, spec)
+
+
+# transfer function to convert Primary DHI Index to Spec=Sens
+def dhip_to_spec(x):
+    return -.697*x**2 + 1.584*x + 0.053
+
+# squashing function to create Sens from Spec
+def spec_to_sens(s, p):
+    return (p-.05) * (((1-s)-s) / (.82-.05)) + s
+
+# general definitions
+# define columns
+grade_types = ['A', 'B', 'C', 'D']
+weight_types = ['W1', 'W2', 'W3']
+
+def initialize_dhimatrix():
+    # INITIALIZE DATAFRAME
+    # assign bmt names, ids, extended descriptions
+    dhim_names = {
+        # BACKGROUND TAB
+        'DHI_BACK_1': ['BUR', 'Burial depth'],
+        'DHI_BACK_2': ['PORO', 'Porosity range'],
+        'DHI_BACK_3': ['FLUID', 'HC type'],
+        'DHI_BACK_4': ['AGE', 'Age of target'],
+        'DHI_BACK_5': ['DHIP', 'DHI play'],
+        'DHI_BACK_6': ['SFACIES', 'Seismic facies'],
+        'DHI_BACK_7': ['PITFALL', 'Possible pitfalls'],
+        # DATA QUALITY TAB
+        'DHI_DQ_1': ['SEISD', 'Seismic database'],
+        'DHI_DQ_2': ['SEISQ', 'Seismic data quality'],
+        'DHI_DQ_3': ['RPM', 'Rock physics model'],
+        'DHI_DQ_4': ['CLASS', 'AVO class'],
+        # AMPLITUDE ANOMALY TAB
+        'DHI_AA_1': ['AMPC', 'Amplitude change'],
+        'DHI_AA_2': ['ACONS', 'Amplitude consistency'],
+        'DHI_AA_3': ['MODM', 'Seismic signature'],
+        'DHI_AA_4': ['STRFIT', 'Amplitude structural fit'],
+        # AVO TAB
+        'DHI_AVO_1': ['AVOM', 'AVO analysis method'],
+        'DHI_AVO_2': ['AVOA', 'AVO change & consistency'],
+        'DHI_AVO_3': ['AVOVM', 'AVO signature'],
+        'DHI_AVO_4': ['AVOFIT', 'AVO structural fit'],
+        # OTHER DHIS TAB
+        'DHI_OTHER_1': ['FSPOT', 'Flat spot'],
+        'DHI_OTHER_2': ['PHASE', 'Phase change'],
+        'DHI_OTHER_3': ['LOWF', 'Low frequency anomaly'],
+        'DHI_OTHER_4': ['PULLD', 'Pull-down'],
+        'DHI_OTHER_5': ['CHIM', 'Gas chimneys'],
+    }
+    
+    # assign scores
+    # i.e. the numerical equivalent of grades A, B, C, D for each characteristic
+    # TAKEN FROM DM3-PAPA_v22-00.xlsx
+    scores = np.array([
+        [4, 3, 2, 1],
+        [4, 3, 2, 1],
+        [4, 3, 2, 1],
+        [4, 3, 2, 1],
+        [2, 5, 7, 10],
+        [5, 7, 9, 10],
+        [3, 5, 9, 10],
+        [6, 7, 8, 10],
+        [2, 5, 8, 10],
+        [0, 6, 8, 10],
+        [0, 8, 10, 9],
+        [0, 5, 8, 10],
+        [0, 3, 7, 10],
+        [0, 4, 7, 10],
+        [0, 4, 7, 10],
+        [0, 4, 7, 10],
+        [0, 2, 7, 10],
+        [0, 2, 7, 10],
+        [0, 4, 6, 10],
+        [2, 5, 7, 10],
+        [4, 5, 7, 10],
+        [5, 6, 8, 10],
+        [5, 6, 8, 10],
+        [2, 5, 7, 10],
+    ])
+
+    # assign class weights
+    # i.e. modifiers for scores on the basis of selected AVO class
+    # TAKEN FROM DM3-PAPA_v22-00.xlsx
+    class_weights = np.array([
+        [np.nan, np.nan, np.nan],
+        [np.nan, np.nan, np.nan],
+        [np.nan, np.nan, np.nan],
+        [np.nan, np.nan, np.nan],
+        [0.7,1.0,1.2],
+        [1.0,1.4,1.2],
+        [1.4,1.7,1.9],
+        [1.3,1.2,1.1],
+        [0.7,1.4,1.0],
+        [1.0,1.9,1.4],
+        [1.0,1.2,1.4],
+        [1.4,1.9,1.0],
+        [1.4,0.5,1.9],
+        [1.3,0.7,1.0],
+        [1.9,1.4,1.9],
+        [1.0,1.4,1.2],
+        [1.3,1.0,1.0],
+        [1.0,1.0,0.7],
+        [1.0,1.0,1.0],
+        [0.1,0.5,0.5],
+        [0.1,0.5,0.5],
+        [0.1,0.5,0.5],
+        [0.1,0.4,0.7],
+        [1.0,1.0,1.0],
+    ])
+    
+    # assemble everything into a DataFrame
+    tmp1 = pd.DataFrame.from_dict(dhim_names, orient='index', columns=['ID', 'Description'])
+
+    # extract number of characteristics to grade: 24
+    ncar = tmp1.shape[0]
+
+    # make temporary matrix with all other components of the "engine"
+    cols = ['A', 'B', 'C', 'D', 'W1', 'W2', 'W3', 'CF', 'MAXWF', 'GRADE', 'POWER']
+    nans = np.full(shape=(ncar, len(cols)),fill_value=np.nan)
+    tmp2 = pd.DataFrame(nans, index=tmp1.index, columns=cols)
+
+    # combine the two DataFrames 
+    dm = pd.concat([tmp1, tmp2], axis=1)
+
+    # fill in grade "scores", i.e. associate numerical values (scores)
+    # to selectable grades (A, B, C, D)
+    # fill in weight "scores"
+    for i, nn in enumerate(dm['ID']):
+        dm.loc[dm['ID']==nn, grade_types] = scores[i, :]
+        dm.loc[dm['ID']==nn, weight_types] = class_weights[i, :]
+    
+    return dm
+
+def update_dhi_matrix(dhimatrix, grades):    
+ 
+    dm = dhimatrix.copy()
+ 
+    # define list of IDs
+    # all_ids = all IDs for entire DHI Matrix
+    # power_ids = only the IDs where POWER is calculated
+    # backgr_ids = background IDs
+    # dataqu_ids = data quality IDs
+    class_ids = ['BUR', 'PORO', 'FLUID','AGE']
+    all_ids = dm['ID'].to_numpy()
+    power_ids = dm.loc[~dm['ID'].isin(class_ids), 'ID'].to_numpy()
+    backgr_ids = dm.loc[dm.index.str.startswith('DHI_BACK'), 'ID'].to_numpy()
+    dataqu_ids = dm.loc[dm.index.str.startswith('DHI_DQ'), 'ID'].to_numpy()
+    
+    # primary_ids = to calculate primary DHI index
+    incl_backgr = dm['ID'].isin(backgr_ids)
+    incl_dataqu = dm['ID'].isin(dataqu_ids)
+    tmp = dm.loc[~(incl_backgr.values | incl_dataqu.values), 'ID'].to_numpy()
+    index_AVOM = np.argwhere(tmp=='AVOM')
+    primary_ids = np.delete(tmp, index_AVOM)
+
+    #>>> USER INPUT: grades
+    dm['GRADE'] = grades
+
+    # convert grades to numbers (POWER) for first 4 background characteristics
+    for nn in class_ids:
+        assigned_grade = dm.loc[dm['ID']==nn, 'GRADE']
+        score_grade = dm.loc[dm['ID']==nn, assigned_grade].to_numpy().flatten()
+        dm.loc[dm['ID']==nn, 'POWER' ] = score_grade
+
+    # COMPCL: computed avoclass from background characteristics 
+    # to be used when avo class is unknown (grade A in DHI_DQ_4 or 'CLASS')
+    coeffs = [0.32, 0.25, 0.20, 0.05]
+    class_scores = dm.loc[dm['ID'].isin(class_ids), 'POWER'].to_numpy()
+    computed_class = np.round(class_scores * coeffs)
+    computed_class = int(np.round(np.sum(coeffs * class_scores)))
+
+    # get avo class from DHI_DQ_4 ('CLASS')
+    assigned_avoclass = dm.loc[dm['ID']=='CLASS', 'GRADE']
+    if 'A' in assigned_avoclass.values:
+        avoclass = computed_class
+    elif 'B' in assigned_avoclass.values:
+        avoclass = 1
+    elif 'C' in assigned_avoclass.values:
+        avoclass = 2
+    elif 'D' in assigned_avoclass.values:
+        avoclass = 3
+
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # deactivate a few attributes on the basis of context
+    # i.e. a few conditions that make certain attributes not reliable
+
+    # assign context flags
+    dm['CF'] = True
+
+    # if SFACIES=A (seismic facies is chaotic) then ACONS and MODM=0
+    condition = dm.loc[dm['ID']=='SFACIES', 'GRADE'].isin(['A'])
+    dm.loc[dm['ID']=='ACONS', 'CF'] = np.where(condition, False, True)
+    dm.loc[dm['ID']=='MODM', 'CF'] = np.where(condition, False, True)
+
+    # if AVOM=A (no avo analysis) then all other AVO charact=0
+    condition = dm.loc[dm['ID']=='AVOM', 'GRADE'].isin(['A'])
+    dm.loc[dm['ID']=='AVOA', 'CF'] = np.where(condition, False, True)
+    dm.loc[dm['ID']=='AVOVM', 'CF'] = np.where(condition, False, True)
+    dm.loc[dm['ID']=='AVOFIT', 'CF'] = np.where(condition, False, True)
+
+    # if SFACIES=D (seismic facies is single loop reflection) then FSPOT=0
+    condition = dm.loc[dm['ID']=='SFACIES', 'GRADE'].isin(['D'])
+    dm.loc[dm['ID']=='FSPOT', 'CF'] = np.where(condition, False, True)
+
+    # if FLUID=C, D (oil) or PORO=D (low porosity) then PULLD=0
+    fluid_is_oil = dm.loc[dm['ID']=='FLUID', 'GRADE'].isin(['C', 'D']) 
+    low_poro = dm.loc[dm['ID']=='PORO', 'GRADE'].isin(['D']) 
+    condition = fluid_is_oil.values or low_poro.values
+    dm.loc[dm['ID']=='PULLD', 'CF'] = np.where(condition, False, True)
+
+    # if FSPOT=A (flat spot expected but not found) then CHIM=0
+    condition = dm.loc[dm['ID']=='FSPOT', 'GRADE'].isin(['A'])
+    dm.loc[dm['ID']=='CHIM', 'CF'] = np.where(condition, False, True)
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    # calculate MAXWF and make it 0 if context flag is False
+    dm['MAXWF' ] = dm[weight_types].max(axis=1) * dm['CF']
+
+    # calculate POWER = scores * avo weight * context flags
+    # select all IDs except the first 4 background characteristics
+    for nn in power_ids:
+        selec_id = dm['ID']==nn
+        assigned_grade = dm.loc[selec_id, 'GRADE']
+        score_grade = dm.loc[selec_id, assigned_grade].to_numpy().flatten()   
+        weight = dm.loc[selec_id, 'W'+str(avoclass)].to_numpy().flatten()
+        context_flag = dm.loc[selec_id, 'CF'].to_numpy().flatten()   
+        dm.loc[selec_id, 'POWER' ] = score_grade * weight * context_flag /10
+
+    # PRIMARY DHI INDEX
+    above = dm.loc[dm['ID'].isin(primary_ids), 'POWER'].sum()
+    below = dm.loc[dm['ID'].isin(primary_ids), 'MAXWF'].sum()
+    primary_dhi_index = above / below
+
+    # EXTENDED DHI INDEX
+    above = dm.loc[dm['ID'].isin(power_ids), 'POWER'].sum()
+    below = dm.loc[dm['ID'].isin(power_ids), 'MAXWF'].sum()
+    extended_dhi_index = above / below
+
+    pitfall_1_ids = ['DHIP', 'SFACIES', 'SEISQ', 'CLASS', 'AVOM']
+    above = dm.loc[dm['ID'].isin(pitfall_1_ids), 'POWER'].sum()
+    below = dm.loc[dm['ID'].isin(pitfall_1_ids), 'MAXWF'].sum()
+    pitfall_1_index = 1-(above / below)
+
+    pitfall_2_ids = ['PITFALL', 'SEISD', 'RPM', 'AVOM']
+    above = dm.loc[dm['ID'].isin(pitfall_2_ids), 'POWER'].sum()
+    below = dm.loc[dm['ID'].isin(pitfall_2_ids), 'MAXWF'].sum()
+    pitfall_2_index = 1-(above / below)
+
+    pitfall_index = np.mean([pitfall_1_index, pitfall_2_index])
+
+    return dm, primary_dhi_index, pitfall_1_index, pitfall_2_index, pitfall_index
+
+def update_pos(pos_input, primary_dhi_index, pitfall_index, fancy=True):
+
+    # build engine dataframe "prb"
+    riskelem = ['RES', 'SEA', 'SRC', 'TRP', 'CHG']
+    
+    #>>> USER INPUT: probabilities risk elements
+    pos_elem = dict(zip(riskelem, pos_input))
+    prb = pd.DataFrame.from_dict(pos_elem, orient='index', columns=['posg'])
+
+    # add specificity
+    prb['spec'] = dhip_to_spec(primary_dhi_index)
+
+    # add pitfall index modified for risk elements
+    if fancy:
+        coeff_pitfall_index = [.7, .5, 1, .3, 1]
+        prb['pitf'] = pitfall_index**coeff_pitfall_index 
+    else:
+        prb['pitf'] = pitfall_index
+ 
+    # add sensitivity
+    prb['sens'] = spec_to_sens(prb['spec'], prb['pitf'])
+
+    # calculate POS with Bayes on risk elements
+    prb['pos'] = posterior_probability(prb['posg'], prb['sens'], prb['spec'])
+
+    # calculate POSg and updated POS
+    posg = prb['posg'].product()
+    pos = prb['pos'].product()
+
+    return posg, pos, prb
+
